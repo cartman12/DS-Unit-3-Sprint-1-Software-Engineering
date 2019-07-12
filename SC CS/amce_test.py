dad228c4bf1b250cd822aa1c50fbca59e84783cc
cartman12 <mhd.ali.nasser@gmail.com>
2019-07-12 22:23:03 +0300

    SC

--- SC CS/amce_test.py
+++ SC CS/amce_test.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import unittest
 from acme import Product
 from acme_report import generate_products, adjectives, nouns
@@ -11,7 +10,17 @@ class AcmeProductTests(unittest.TestCase):
         """Test default product price being 10."""
         prod = Product('Test Product')
         self.assertEqual(prod.price, 10)
+        self.assertEqual(prod.explode(), ('...fizzle') | '...boom!' | '...BABOOM!!'))
+        self.assertEqual(prod.stealability(), ('Not stealable') | 'Kind stealable' | 'Not so stealable''),)
 
+class AcmeReportTests(unittest.TestCase):
+    """Making sure counts and names are right"""
+    def test_default_num_products(self):
+      prod = generate_products()
+      self.assertCountEqual(prod.names, 30)
+    def test_legal_names(self):
+      prod = generate_products()
+      self.assertIn(prod.names,adjectives.append(nouns))
 
 if __name__ == '__main__':
     unittest.main()
